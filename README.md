aabeaca5ef77500ba15e24b4dfe2bffd177a8ca9
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-07-28 16:04:46 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Midterm_BTC1855
+
+This file will contain my plan in tacking the task
+
+Step 1: load the datasets into the file
+Step 2: Inspect and clean each of the datasets
---
+++ main.R
@@ -0,0 +1,10 @@
+# Mikael Gouwtama
+# 1007128127
+# BTC 1855H Midterm Project 
+
+# Read the 3 datasets
+
+station <- read.csv("./datasets/station.csv")
+trip <- read.csv("./datasets/trip.csv")
+weather <- read.csv("./datasets/weather.csv")
+
